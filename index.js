5ded4717be451978ddad6f465cb0ddce15a7d013
Andriy <andriystarko1996@gmail.com>
2020-11-14 17:49:40 +0200

    create emailGenerate module(function)

---
+++ index.js
@@ -0,0 +1,3 @@
+
+let generateEmails = require('./src/emails_generate');
+
---
+++ src/emails_generate.js
@@ -0,0 +1,19 @@
+
+
+function generateEmails(name, surname, domain){
+    let result = new Set();
+    let specialСharacters = ['!', '~', '#', '|', '&', '.', '-', '_', ''];
+    while (result.size < 50){
+        let rname = getRandomInt(0, 4) ? name : ''; // email with name or without (1/4 probability that email will not have a name)
+        let rsurname = rname ? [surname, surname.slice(0, 1), ''][getRandomInt(0, 3)] : surname; // generate surname
+        let specialChar = rname && rsurname ? specialСharacters[Math.floor(Math.sqrt(Math.random() * 81))] : ''; // select the special symbol using function rule sqrt(x)
+        let rnumber = getRandomInt(0, 4) ? getRandomInt(1, 100) : ''; // add random number to email (in 3/4 probability number will be added)
+        result.add(rname + specialChar + rsurname + rnumber + '@' + domain)
+    }
+    function getRandomInt(minInt, maxInt) {
+        return Math.floor(Math.random() * (maxInt - minInt)) + minInt;
+    }
+    return Array.from(result)
+}
+
+module.exports = generateEmails;
