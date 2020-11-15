6b8f91c93360ff12912623204cacb759cda38965
Andriy <andriystarko1996@gmail.com>
2020-11-15 20:42:01 +0200

    made mx DNS LOOkUP and put together all modules in index.js

--- index.js
+++ index.js
@@ -6,4 +6,25 @@ require('dotenv').config();
 
 let tld = ['com','uk'];
 
+fs.readFile('./src/input.txt', 'utf-8', (err, data) => {
+    if (err) {
+        console.log(err.message);
+        return
+    }
+    let emailData = data.split(',');
+    tld.forEach((d) => {
+        let domain = `${emailData[2]}.${d}`;
+        //MX DNS lookup
+        dns.resolveMx(domain, (err, addresses) => {
+            if (err) {
+                console.log(err.message + ' in resolveMx');
+                return
+            };
+            // generate process.env.NUNBER_EMAILS random emails
+            let emails = generateEmails(emailData[0], emailData[1], domain, process.env.NUMBER_EMAILS);
+            // checks emails and writes down available emails in src/output.txt
+            checkEmails(emails, addresses[0].exchange);
+        });
+    });
+})
 
